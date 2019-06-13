dde22c2f8d2962fed681fe13e71f982923a100d7
jingxuelong <jingxuelong0037@gmail.com>
2019-06-13 13:21:49 +0800

    1.first commit

---
+++ JingMY_Swift/JingMY_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JingMY_Swift
+//
+//  Created by jing_mac on 2019/6/13.
+//  Copyright © 2019 jing_mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JingMY_Swift/JingMY_SwiftTests/JingMY_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  JingMY_SwiftTests.swift
+//  JingMY_SwiftTests
+//
+//  Created by jing_mac on 2019/6/13.
+//  Copyright © 2019 jing_mac. All rights reserved.
+//
+
+import XCTest
+@testable import JingMY_Swift
+
+class JingMY_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
